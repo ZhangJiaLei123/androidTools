264bd8e8f6ceab0e2bda64b90d601e3ebaa14c16
zjl <Zhang.jialei@outlook.com>
2019-02-20 11:03:00 +0800

    增加时间工具

---
+++ mUnitCode/mTimeUnit/src/main/java/com/bigbai/mtimeunit/CalendarUnit.java
@@ -0,0 +1,32 @@
+package com.bigbai.mtimeunit;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * 时间工具
+ */
+public class CalendarUnit {
+    static final int onelyTime = 0;
+    static final int onelyDate = 2;
+
+
+    public static String getTime(int Model){
+        Calendar cal;
+        cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+
+        switch (Model){
+            case onelyTime:
+                return cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + ":" + cal.get(Calendar.MILLISECOND);
+
+            case onelyDate:
+                return cal.get(Calendar.YEAR) + "/" + cal.get(Calendar.MONTH) + cal.get(Calendar.DATE) + " " + cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + ":" + cal.get(Calendar.MILLISECOND);
+
+                default:
+                    return cal.get(Calendar.HOUR) + ":" + cal.get(Calendar.MINUTE) + ":" + cal.get(Calendar.SECOND) + ":" + cal.get(Calendar.MILLISECOND);
+
+        }
+
+    }
+}
