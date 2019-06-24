9629e37fbea8a40b15659fb5c498242e7e5928c3
Bai <Zhang.jialei@outlook.com>
2019-06-24 15:13:58 +0800

    更新

--- mUnitCode/mudp/src/main/java/com/blxt/mudp/MUdpClient.java
+++ mUnitCode/mudp/src/main/java/com/blxt/mudp/MUdpClient.java
@@ -1,5 +1,7 @@
 package com.blxt.mudp;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -12,12 +14,13 @@ import java.net.InetAddress;
  * @note Created by com.blxt.mudp.
  */
 public class MUdpClient {
+    public static String TAG = "MUdpClient";
     /** 最大缓存字节 */
     private int MAX_BUFF_LENG = 1024;
     private String addr = "";
 
     int prot = 8080;
-    DatagramSocket socket = null;
+    public DatagramSocket socket = null;
     InetAddress serverAddress = null;
 
     DatagramPacket packetRec = null;
@@ -39,70 +42,93 @@ public class MUdpClient {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        if(socket == null){
+            Log.e(TAG,"socket创建失败");
+        }
     }
-
 
 
     /**
      * 发送字节
      * @param data
      */
-    public void sendByte(final byte data[]){
+    public void sendByte(byte data[]){
+        try{
+            DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, prot);
+            socket.send(packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送字节
+     * @param data
+     */
+    public void sendByteThread(final byte data[]){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    DatagramPacket packet = new DatagramPacket(data, data.length, serverAddress, prot);
-                    socket.send(packet);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                sendByte(data);
             }
         }).start();
     }
 
+    /**
+     *  接收指定长度的字节
+     * @param leng
+     * @param fal
+     */
+    public byte[] receiveMessage(int leng, boolean fal){
+        byte[] data = null;
+        if(socket == null){
+            return data;
+        }
+        try {
+            byte[] receBuf = new byte[MAX_BUFF_LENG];
+            packetRec = new DatagramPacket(receBuf, receBuf.length);
+            socket.receive(packetRec);
+            if(callBack != null){ // 收到消息
+                int len = 0;
+                if(fal) { len = leng;
+                }
+                else{ len = packetRec.getLength();
+                }
+                data = new byte[len];
+                // 截取有效长度
+                System.arraycopy(packetRec.getData(), 0, data, 0, len);
+                callBack.receiveData(data); // 发送给回调接口
+            }
+        } catch (IOException e) {
+            data = null;
+            e.printStackTrace();
+        }
+        return data;
+    }
 
     /**
      * 接收指定长度的字节
      * @param leng
      */
-    public void receiveMessage(final int leng, final boolean fal){
+    public void receiveMessageThread(final int leng, final boolean fal){
         new Thread() {
             @Override
             public void run() {
                 while (isGetReceive) {
-                    try {
-                        byte[] receBuf = new byte[MAX_BUFF_LENG];
-                        packetRec = new DatagramPacket(receBuf, receBuf.length);
-                        socket.receive(packetRec);
-                        if(callBack != null){ // 收到消息
-                            byte[] data;
-                            int len = 0;
-                            if(fal) { len = leng;
-                            }
-                            else{ len = packetRec.getLength();
-                            }
-                            data = new byte[len];
-                            // 截取有效长度
-                            System.arraycopy(packetRec.getData(), 0, data, 0, len);
-                            callBack.receiveData(data); // 发送给回调接口
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                    receiveMessage(leng, false);
                 }
             }
         }.start();
     }
 
-    public void receiveMessage(int leng) {
-        receiveMessage(leng, true);
+    public void receiveMessageThread(int leng) {
+        receiveMessageThread(leng, true);
     }
     /**
      * 开始接受消息
      */
-    public void receiveMessage() {
-        receiveMessage(0, false);
+    public void receiveMessageThread() {
+        receiveMessageThread(0, false);
     }
 
 
